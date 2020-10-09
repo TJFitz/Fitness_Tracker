b2e2195a2df9fc4d245930b3d3f9ae2323b55b37
TJFitz <tjfitz@comcast.net>
2020-10-09 16:01:11 -0400

    functionality complete

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/api_routes")(app);
+require("./routes/html_routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
