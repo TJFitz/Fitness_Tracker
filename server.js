544adff950dc23f26ff09044841b4e620fdff474
TJFitz <tjfitz@comcast.net>
2020-10-09 17:25:22 -0400

    making things work

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ app.use(express.static("public"));
 require("./routes/api_routes")(app);
 require("./routes/html_routes")(app);
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/workout",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
